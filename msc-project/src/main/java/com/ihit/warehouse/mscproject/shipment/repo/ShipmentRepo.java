4b1d3d67365f21a02eca7f1e57227ba1434aa25e
panthaistiaque <panthaistiaque@gmail.com>
2021-03-12 17:11:50 +0600

    20210312.01

--- msc-project/src/main/java/com/ihit/warehouse/mscproject/shipment/repo/ShipmentRepo.java
+++ msc-project/src/main/java/com/ihit/warehouse/mscproject/shipment/repo/ShipmentRepo.java
@@ -1,11 +1,17 @@
 package com.ihit.warehouse.mscproject.shipment.repo;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,14 +28,23 @@ public class ShipmentRepo {
         this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(this.dataSource);
     }
 
-    public int saveShipment(Map<String, Object> shipment) {
+    public Integer saveShipmentMaster(Map<String, Object> shipment) {
         String insertSql = "INSERT INTO  shipment_master ( suppliers_id, remarks, requested_date) VALUES ( :suppliers_id, :remarks, :requested_date) ";
-        Map<String, Object> map = new HashMap<>();
-
-        map.put("suppliers_id", shipment.get("supplier"));
-        map.put("remarks", shipment.get("rmarks"));
-        map.put("requested_date", shipment.get("requested_date"));
+//        Map<String, Object> map = new HashMap<>();
+        MapSqlParameterSource map = new MapSqlParameterSource();
+        map.addValue("suppliers_id", shipment.get("supplier"));
+        map.addValue("remarks", shipment.get("rmarks"));
+        map.addValue("requested_date", shipment.get("requested_date"));
         System.out.println(map);
-        return namedParameterJdbcTemplate.update(insertSql, map);
+        KeyHolder holder = new GeneratedKeyHolder();
+        namedParameterJdbcTemplate.update(insertSql, map,holder, new String[]{"id"});
+
+        return  holder.getKey().intValue();
+    }
+    public int[] saveShipmentDetails(List<Map<String, Object>> shipment) {
+        SqlParameterSource[] batch = SqlParameterSourceUtils.createBatch(shipment.toArray());
+        int[] updateCounts = namedParameterJdbcTemplate.batchUpdate(
+                "INSERT INTO shipment_details(shipment_master_id, type, name, size, qty)  VALUES (:orderMasterId, :type, :name, :size, :qty)", batch);
+        return updateCounts;
     }
 }
--- msc-project/src/main/java/com/ihit/warehouse/mscproject/shipment/service/ShipmentService.java
+++ msc-project/src/main/java/com/ihit/warehouse/mscproject/shipment/service/ShipmentService.java
@@ -3,7 +3,9 @@ package com.ihit.warehouse.mscproject.shipment.service;
 import com.ihit.warehouse.mscproject.shipment.repo.ShipmentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,8 +16,16 @@ public class ShipmentService {
     @Autowired
     ShipmentRepo shipmentRepo;
 
+    @Transactional
     public Map<String, Object> saveShipment(Map<String, Object> shipment) {
-        shipmentRepo.saveShipment(shipment);
+        Integer orderMasterId = shipmentRepo.saveShipmentMaster(shipment);
+        List<Map<String, Object>>  maps = (List<Map<String, Object>>) shipment.get("item");
+        System.out.println(orderMasterId);
+        for (Map<String, Object> obj: maps) {
+            obj.put("orderMasterId", orderMasterId);
+        }
+        int[] detailsId= shipmentRepo.saveShipmentDetails(maps);
+        System.out.println(detailsId);
         return null;
     }
 }
