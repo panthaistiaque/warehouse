e1e690f399ab56975aab6d5583b990793a89f94e
panthaistiaque <33906293+panthaistiaque@users.noreply.github.com>
2021-10-21 00:44:36 +0600

    ee

--- msc-project/src/main/java/com/ihit/warehouse/mscproject/receive/ReceiveCtrl.java
+++ msc-project/src/main/java/com/ihit/warehouse/mscproject/receive/ReceiveCtrl.java
@@ -4,13 +4,14 @@ import com.ihit.warehouse.mscproject.config.AppProperty;
 import com.ihit.warehouse.mscproject.order.Order;
 import com.ihit.warehouse.mscproject.users.DataBind.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 /**
  * Created by User on 9/22/2021.
@@ -31,5 +32,15 @@ public class ReceiveCtrl extends AppProperty {
     @PostMapping(value = "/receive-details/{id}")
     private Received receiveDetail(@PathVariable("id") Integer id) {
         return receivedService.getById(id);
+    }
+
+    @PostMapping(value = "/saveReceive")
+    private ModelAndView saveReceiveConfirm(ModelAndView modelAndView, RedirectAttributes redirect, @RequestBody Received req) {
+//        order = orderService.save(order);
+//        if(order.getId()>0){
+//            redirect.addFlashAttribute("mess", "Order created successfully");
+//        }
+        modelAndView.setViewName("redirect:/receive_list");
+        return modelAndView;
     }
 }
